fe68ec42745c012e273fd1d3e1b281749e51c9f3
Downquark7 <mail2emn@gmail.com>
2017-03-08 10:22:53 -0600

    Update antiSpin.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/antiSpin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/antiSpin.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//may or may not be needed:
+import com.stormbots;
 
 /**
  * Created by mail2 on 11/7/2016.
@@ -12,32 +16,107 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class antiSpin extends LinearOpMode {
     public robotconfig robot = new robotconfig();
     public preciseMovement p = new preciseMovement();
-    public pidController pid = new pidController(0, 0, 1);
+    
     private double forward = 0;
     private double right = 0;
     private double spin = 0;
+    
+    private double pGain = 0;
+    private double iGain = 0;
+    private double dGain = 0;
+    private double output = 0;
+    
+    MiniPID pid = MiniPID(pGain, iGain, dGain);
+    pid.setOutputLimits(-1, 1);
+    pid.setDirection(true);
+    
+    private boolean positiveOutput = false;
+    private int timeCount = 0;
+    private int ntimeCount = 0;
+    
+    private ElapsedTime loopTimer = new ElapsedTime();
+    private double loopTime = 0;
+    private double waitTime = 50;
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(this);
         idle();
         waitForStart();
-        while (opModeIsActive() && pid.tunning) {
-            forward = -gamepad1.left_stick_y * 0.6;
-            right = gamepad1.left_stick_x * 0.6;
-//            spin = gamepad1.right_stick_x * 0.3;
-            pid.pTune();
-            robot.move(forward, right, spin - pid.getPID((robot.getCurrentAngle() + 360) % 360, telemetry));
-            telemetry.update();
-            idle();
-        }
         while (opModeIsActive()) {
-            forward = -gamepad1.left_stick_y;
-            right = gamepad1.left_stick_x;
-            spin = gamepad1.right_stick_x;
-            robot.move(forward, right, spin - pid.getPID((robot.getCurrentAngle() + 360) % 360, telemetry));
+            
+            if(gamepad1.a) {
+                pGain -= gamepad1.left_stick_y / 100;
+                pid.setP(pGain);
+            } else if(gamepad1.b) {
+                iGain -= gamepad1.left_stick_y / 100;
+                pid.setI(iGain);
+            } else if(gamepad1.y) {
+                dGain -= gamepad1.left_stick_y / 100;
+                pid.setD(dGain);
+            } else if(gamepad1.left_bumper) {
+                iGain = 0;
+                dGain = 0;
+                pGain = pGain / 0.60;
+                pid.setP(pGain);
+                pid.setI(iGain);
+                pid.setD(dGain);
+            } else if(gamepad1.right_bumper) {
+                iGain = 1.2 * pGain / timeCount;
+                dGain = 3 * pGain * timeCount / 40;
+                pGain = pGain * 0.60;
+                //https://en.wikipedia.org/wiki/Ziegler%E2%80%93Nichols_method
+                pid.setP(pGain);
+                pid.setI(iGain);
+                pid.setD(dGain);
+                while(gamepad1.right_bumper) {
+                    idle();
+                }
+            } else {
+                forward = -gamepad1.left_stick_y;
+                right = gamepad1.left_stick_x;
+            }
+            
+            spin += gamepad1.right_stick_x;
+            
+            output = pid.getOutput(robot.getCurrentAngle(), spin);
+            robot.move(forward, right, output);
+            
+            ntimeCount++;
+            
+            if(positiveOutput) {
+                
+            if (output < 0) {
+            positiveOuput = false;
+        }
+                
+            } else {
+                
+            if (output > 0) {
+            positiveOuput = true;
+            timeCount = ntimeCount;
+            ntimeCount = 0;
+        }
+                
+            }
+            
+            telemetry.addData("spin", "%f", spin);
+            telemetry.addData("output", "%f", output);
+            telemetry.addLine();
+            telemetry.addData("timeCount", "%d", timeCount);
+            telemetry.addData("pGain", "%f", pGain);
+            telemetry.addData("iGain", "%f", iGain);
+            telemetry.addData("dGain", "%f", dGain);
+            telemetry.addLine();
+            telemetry.addData("loopTime", "%.0f", loopTime);
+            telemetry.addData("waitTime", "%.0f", waitTime);
             telemetry.update();
-            idle();
+            
+            loopTime = loopTimer.milliseconds();
+            waitTime = Math.max(0, 50 - loopTime);
+            loopTimer.reset();
+            
+            sleep(waitTime);
         }
     }
 }
